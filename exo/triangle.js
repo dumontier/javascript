a5ee493bff998ebf6d5055a7416a56edf28dba2c
dumontier <jean-philippe.dumontier@lepoles.com>
2018-08-03 16:55:32 +0200

    javasc

---
+++ exo/triangle.js
@@ -0,0 +1,19 @@
+// boucle for
+
+var diese = "";
+
+for(var i = 1; i < 8; i++){
+    diese += "#";
+    // diese = diese + "#";
+    console.log(diese);
+}
+
+// boucle while
+
+var star ="";
+var CompteurDeTour = 1;
+while (CompteDeTour <= 7) {
+    star = star + "*";
+    console.log(star);
+    CompteurDeTour++;
+}
