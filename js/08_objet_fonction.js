d33b141a8af594d9886ec59ed5fedc2d0e0add36
dumontier <jean-philippe.dumontier@lepoles.com>
2018-08-20 16:43:34 +0200

    java

--- js/08_objet_fonction.js
+++ js/08_objet_fonction.js
@@ -65,51 +65,93 @@ console.log(contacts.prenom);
 * Essayer de trouver comment parcourir un tableau contenant des objets
 *
 */
-var stagiaires = [
-    {
-        "prenom": "Elies",
-        "code": "JavaScript"
+// var stagiaires = [
+//     {
+//         "prenom": "Elies",
+//         "code": "JavaScript"
 
-    },
-    {
-        "prenom": "Mohamed",
-        "code": "Html"
-    },
-    {
-        "prenom": "Jhordan",
-        "code": "Css"
-    },
-    {
-        "prenom": "Alpha",
-        "code": "Video"
-    },
-    {
-        "prenom": "Jean-Philippe",
-        "code": "Css"
-    },
-    {
-        "prenom": "Luc",
-        "code": "POO"
-    },
-    {
-        "prenom": "Layla",
-        "code": "PHP"
-    },
-    {
-        "prenom": "Sabuj",
-        "code": "Angular"
-    },
-    {
-        "prenom": "Arnaud",
-        "code": "Symfony"
-    },];
+//     },
+//     {
+//         "prenom": "Mohamed",
+//         "code": "Html"
+//     },
+//     {
+//         "prenom": "Jhordan",
+//         "code": "Css"
+//     },
+//     {
+//         "prenom": "Alpha",
+//         "code": "Video"
+//     },
+//     {
+//         "prenom": "Jean-Philippe",
+//         "code": "Css"
+//     },
+//     {
+//         "prenom": "Luc",
+//         "code": "POO"
+//     },
+//     {
+//         "prenom": "Layla",
+//         "code": "PHP"
+//     },
+//     {
+//         "prenom": "Sabuj",
+//         "code": "Angular"
+//     },
+//     {
+//         "prenom": "Arnaud",
+//         "code": "Symfony"
+//     },];
 
-for(etudiants = 0; etudiants <= stagiaires.length;
-    etudiants++){
-        console.log("Stagiaires " + etudiants)
-        console.log(stagiaires[etudiants].prenom)
-        console.log("adore " + stagiaires[etudiants].code)
+// for(etudiants = 0; etudiants <= stagiaires.length;
+//     etudiants++){
+//         console.log("Stagiaires " + etudiants)
+//         console.log(stagiaires[etudiants].prenom)
+//         console.log("adore " + stagiaires[etudiants].code)
 
+//     }
+
+/*
+*Fonctions
+*/ 
+// une fonction est une série d'instructions contenues dans des { accolades}
+// elle porte le nom que je lui donne 
+// à chaque fois que j'en ai besoin je l'APPELLE
+// je déclare une fonction avec le mot-clé function
+
+// une fonction anonyme ne prend aucun paramètre
+    //  =>elle n'utilisera pas d'information personnalisée
+    function HelloTous(){
+        alert("Bonjour à tous");
     }
+// pour exécuter ma fonction il faut l'appeler :
+HelloTous(); 
+
+// une fonction avec des paramètres ou arguments
+function pause(prenom){
+    document.write("<h2>" + prenom + " part en pause !</h2>");
+}
+// j'appelle ma fonction avec un paramètre
+pause("katty");
+
+function ecrit(e){
+    document.write("<h3>" + e + "</h3>");
+}
+ecrit("= mc2");
+
+/*
+*EXERCICE - calculette
+*Ecrire une fonction qui additionne 2 chiffres passés en paramètres
+*le mot-clé return permet de retourner le résultat des instructions codées
+*/
 
 
+function calculette(nb1, nb2){
+    var resultat = nb1 + nb2;
+    document.write("<h2>le résultat est : " + resultat + "</h2>");
+    return resultat;
+}
+
+calculette(888, 999);
+calculette(50, 10);
